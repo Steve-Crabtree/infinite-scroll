fc432cacec3f880ac0249a885b61e181576370b3
Steve Crabtree <stevecrabtree1965@gmail.com>
2023-05-26 11:44:03 -0400

    final edit

---
+++ script.js
@@ -0,0 +1,72 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const count = 30;
+const apiKey = 'VFxHE6O067ov-KUPYAFTpQcrvDLRMko_vdDb5pzS-F0';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+// Create Elements for links and photos, add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {}
+}
+
+window.addEventListener('scroll', () => {
+    if (
+        window.innerHeight + window.scrollY >=
+            document.body.offsetHeight - 1000 &&
+        ready
+    ) {
+        ready = false;
+        getPhotos();
+    }
+});
+// On Load
+getPhotos();
